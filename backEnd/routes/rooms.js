b59e3e99fc30987f9ed12686ba9b443d22ff6470
NamanSnag <nnag44141@gmail.com>
2023-02-14 10:56:49 +0530

    backend almost done

---
+++ backEnd/routes/rooms.js
@@ -0,0 +1,21 @@
+import express from 'express';
+const roomRouter = express.Router();
+import room from '../controllers/room_Controller.js';
+import { verifyAdmin } from '../utils/jwt_Verify.js';
+
+// add hotel routes
+roomRouter.post('/:hotelId', verifyAdmin, room.addRoom);
+
+// Update hotel routes
+roomRouter.put('/:id', verifyAdmin, room.updateRoom);
+
+// delete hotel routes
+roomRouter.delete('/:id/:hotelId', verifyAdmin, room.deleteRoom);
+
+// get hotel routes
+roomRouter.get('/:id', room.getRoom);
+
+// get all hotels routes
+roomRouter.get('/', room.allRoom);
+
+export default roomRouter;
--- routes/rooms.js
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-const roomRouter = express.Router();
-
-roomRouter.get('/', (req , res)=>{
-    res.send('Room routes');
-})
-
-export default roomRouter;
